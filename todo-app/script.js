4f65f5d3fc9b44423924dd0f9090889b485298f1
rebixpl <rebixcraft@gmail.com>
2021-05-10 22:41:15 +0200

    added todo-app

---
+++ todo-app/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevents website from refreshing after submitting form
+
+  const todo = input.value;
+
+  if (todo) {
+  }
+});

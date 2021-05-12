96db28868207d46644f5c9c0d7f0c8855bf323a5
rebixpl <rebixcraft@gmail.com>
2021-05-12 16:11:53 +0200

    todo-app update

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,11 +1,67 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // prevents website from refreshing after submitting form
 
-  const todo = input.value;
-
-  if (todo) {
-  }
+  addTodo();
 });
+
+function addTodo(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    // left click
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
+
+    // right click
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault(); // prevents website from showing right click context menu
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}

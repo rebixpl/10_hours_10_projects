2bda03ae2b11fb46a16c5e620313c869a6afa6bb
rebixpl <rebixcraft@gmail.com>
2021-05-13 20:14:48 +0200

    movie-app update

--- movie-app/script.js
+++ movie-app/script.js
@@ -4,15 +4,29 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-const main = document.querySelector("main");
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-async function getMoveis() {
-  const resp = await fetch(APIURL);
+// initally get popular movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
 
-  console.log(respData);
-  respData.results.forEach((movie) => {
-    const { poster_path, title, vote_average } = movie;
+  // console.log(respData);
+
+  showMovies(respData.results);
+}
+
+function showMovies(movies) {
+  // clear main
+  main.innerHTML = "";
+
+  // create movie widgets and add each of them to main
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
@@ -26,11 +40,14 @@ async function getMoveis() {
       <h3>${title}</h3>
       <span class="${getClassByRate(vote_average)}">${vote_average}</span>
     </div>
-  `;
+  <div class="overview"> 
+    <h4>Overview:</h4>
+   ${overview}
+  </div>
+    `;
 
     main.appendChild(movieEl);
   });
-  return respData;
 }
 
 function getClassByRate(vote) {
@@ -43,4 +60,14 @@ function getClassByRate(vote) {
   }
 }
 
-getMoveis();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+    search.value = "";
+  }
+});
